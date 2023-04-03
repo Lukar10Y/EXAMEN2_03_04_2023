8f582daf2b1cf73b8f79c8597ce1e05cba8261ca
Lukar10Y <125102994+Lukar10Y@users.noreply.github.com>
2023-04-03 12:22:50 -0400

    Add files via upload

---
+++ ExamenDockUp/Empleados.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosEmpresa
+{
+    public class Empleados
+    {
+        private string _nombre;
+        private string _apellido;
+        private string _cedula;
+
+        public Empleados()
+        {
+            _nombre = "";
+            _apellido = "";
+            _cedula = "";
+        }
+        public Empleados(string nombre, string apellido, string cedula)
+        {
+
+            _nombre = nombre;
+            _apellido = apellido;
+            _cedula = cedula;
+
+        }
+        public string Nombre
+        {
+
+            set
+            {
+                _nombre = value;
+            }
+
+            get
+            {
+                return _nombre;
+            }
+
+        }
+        public string Apellido
+        {
+
+            set
+            {
+                _apellido = value;
+            }
+
+            get
+            {
+                return _apellido;
+            }
+
+        }
+        public string Cedula
+        {
+
+            set
+            {
+                _cedula = value;
+            }
+
+            get
+            {
+                return _cedula;
+            }
+
+        }
+        public static List<Empleados> Lista() { 
+
+        List<Empleados> ListEmpleado = new List<Empleados>();
+
+        Empleados empleado1 = new Empleados("Maria", "Perez", "8.707.502");
+        Empleados empleado2 = new Empleados("Francisco", "Ochoa", "25.266.567");
+        Empleados empleado3 = new Empleados("Luis", "Galindez", "28.692.623");
+
+        ListEmpleado.Add(empleado1);
+        ListEmpleado.Add(empleado2);
+        ListEmpleado.Add(empleado3);
+
+            return ListEmpleado;
+        }
+    }
+}
---
+++ ExamenDockUp/FormMain.cs
@@ -0,0 +1,94 @@
+﻿using borrador;
+using DatosEmpresa;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vender;
+
+namespace ExamenDockUp
+{
+    public partial class FormMain : Form
+    {
+        private List<Usuario> Usuarios = new List<Usuario>();
+        private List<Productos> Productos = new List<Productos>();
+        private FormGestionDatosEmpresa Empresa = new FormGestionDatosEmpresa();
+        private FormLogin Login = new FormLogin();
+        //FormVender Vender = new FormVender();
+        private FormVenderS Vender = new FormVenderS();
+        private FormGestionDeProductos GestionDeProductos = new FormGestionDeProductos();
+        private FormCambiarDatosUsuario Configuracion = new FormCambiarDatosUsuario();
+        private string pathUsers = @"C:\Users\Fran\Documents\VISUAL STUDIO\COMUNNITY\ExamenDockUp\UsersEXAMEN2.json";
+        private string pathFacturas = @"C:\Users\Fran\Documents\VISUAL STUDIO\COMUNNITY\ExamenDockUp\FacturasEXAMEN2.json";
+        private string _pathEmpleados = @"C:\Users\Fran\Documents\VISUAL STUDIO\COMUNNITY\ExamenDockUp\ListaDeEmpleados.json";
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            Login.Path = pathUsers;
+            Configuracion.Path = pathUsers;
+            Vender.PathFacturas = pathFacturas;
+
+            if (Login.ShowDialog()!=DialogResult.OK){
+                Application.Exit();
+            }
+            else
+            {
+                Usuarios = Login.ListaUsuarios; //En esta linea hago "copia de la lista" para simplificar codigo
+                lblNombre.Text = Usuarios[Login.Posicion].Nombre;
+                lblCargo.Text = Usuarios[Login.Posicion].Cargo;
+                Configuracion.Usuarios = Usuarios;
+                Configuracion.Posicion = Login.Posicion;
+                Vender.Vendedor = Usuarios[Login.Posicion];
+                if (Usuarios.Count < 2)
+                {
+                    Usuarios.Add(new Usuario("Vendedor1", "Vendedor1", false));
+                    MessageBox.Show("Ademas se ha creado un usuario No Administrador de prueba\n\nUsuario: Vendedor1\nClave: Vendedor1");
+                }
+                string Json = JsonConvert.SerializeObject(Usuarios.ToArray(), Formatting.Indented);
+                File.WriteAllText(Login.Path, Json);
+                if (Usuarios[Login.Posicion].Cargo!="Administrador")
+                {
+                    gBoxAdmin.Visible = false;
+                }
+            }
+        }
+        private void btnCerrarSesion_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void btnVender_Click(object sender, EventArgs e)
+        {
+            Vender.Productos = GestionDeProductos.ListaProductos;
+            Vender.ShowDialog();
+        }
+        private void rtbConfiguracion_Click(object sender, EventArgs e)
+        {
+            Configuracion.ShowDialog();
+        }
+        private void pbConfiguracion_Click(object sender, EventArgs e)
+        {
+            Configuracion.ShowDialog();
+        }
+        private void btnProductos_Click(object sender, EventArgs e)
+        {
+            GestionDeProductos.ShowDialog();
+        }
+
+        private void btnEmpresa_Click(object sender, EventArgs e)
+        {
+            Empresa.PathEmpleados = _pathEmpleados;
+            Empresa.ShowDialog();
+        }
+    }
+}
